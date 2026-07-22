55ca2f8483b3a8925a61fb60c3462598409346ac
Anthony <anthony12ortega@gmail.com>
2026-07-22 22:02:16 +0100

    chore: increment service worker cache version to day-v21

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Simple offline cache for the app shell.
-const CACHE = "day-v20";
+const CACHE = "day-v21";
 const ASSETS = [
   "./", "./index.html", "./styles.css", "./app.js", "./config.js",
   "./manifest.webmanifest",
